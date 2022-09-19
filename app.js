54075bfd350289cbee3c324d3e2cb33d489401be
StevensonM <110733098+StevensonM@users.noreply.github.com>
2022-09-19 18:19:08 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+
+// Instead of using the above method, we can use a loop to achieve the same result.
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
+
